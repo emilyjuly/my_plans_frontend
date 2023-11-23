810a8a55e65ef5a96a6323dde3caff4096c5583d
July Gomes Domingos <ejuly@minsait.com>
2023-11-23 13:07:35 -0300

    ✨feat: Adicionadas algumas partes na index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
 <template>
-  <header class="flex align-items-center">
-    <Image src="/logo.png" alt="logo" width="50" />
-    <h1>My plans</h1>
+  <header class="flex justify-content-between">
+    <div class="flex align-items-center">
+      <Image src="/logo.png" alt="logo" width="25" class="logo" />
+      <p class="text-sm" style="color: #08d9d6">My plans</p>
+    </div>
+    <div class="flex align-items-center">
+      <Avatar image="/profile.jpg" class="mr-2" shape="circle" />
+      <p class="text-sm">Emily July</p>
+    </div>
   </header>
+  <div class="flex">
+    <nav class="w-3 shadow-3 h-full" style="background-color: red">teste</nav>
+    <main style="background-color: var(--base-color)" class="w-9 h-full">
+      teste
+    </main>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
