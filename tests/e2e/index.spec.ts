6e6e547a497d6166431e59d8f2a13573f14a5189
July Gomes Domingos <ejuly@minsait.com>
2023-11-23 14:49:53 -0300

    ✨feat: Adicionando playwrigth para testes

---
+++ tests/e2e/index.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "@playwright/test";
+
+test("job board test", async ({ page }) => {
+  await page.goto("http://localhost:3000" + "/"); // <-- Nuxt app is running and route '/' is showing.
+
+  // Step 1 - Is Homepage working
+  await expect(page.locator("p").locator("text=teste")).toBeVisible();
+});
